203b7a9d0349cc7ed1103cc476d41c853e6dabdf
Rahul Chauhan <smr.rahul@gmail.com>
2023-02-26 00:25:11 +0530

    initial commit

--- lib/services/konquer_service.dart
+++ lib/services/konquer_service.dart
@@ -26,6 +26,23 @@ class KonquerService {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
     'Accept-Encoding': 'gzip',
   };
+  var headersDelete = {
+    'authority': 'staging-api.konquer.club',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4ODI3MzQwLCJpYXQiOjE2NzcyOTEzNDAsImp0aSI6ImE4MGY0YjBhOGM0NDQzMmJiMzMwOThkNzI5NzMxZDc4IiwidXNlcl9pZCI6MjQ0OX0.C1woukrC1lmIHcy9CbKwPTjEsGVYFMzH4EhlZvvx4h4',
+    'content-type': 'application/json; charset=utf-8',
+    'origin': 'http://localhost:52994',
+    'referer': 'http://localhost:52994/',
+    'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+    'Accept-Encoding': 'gzip',
+  };
   Future<List<Bout>> loadBouts() async {
     Response response = await get(
       Uri.parse(Constants.matchesUrl),
@@ -61,7 +78,7 @@ class KonquerService {
   Future<bool> cancelBout(int matchId) async {
     Response response = await delete(
       Uri.parse(Constants.cancelMatchUrl+matchId.toString()),
-      headers: headers,
+      headers: headersDelete,
     );
     if(response.statusCode==200){
       print(jsonDecode(response.body));
